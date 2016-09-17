e1e64be51b7ebb967902f553dac5c5816373975a
exceltechllc <exceltechllc@yahoo.com>
2016-09-17 00:51:52 -0400

    test

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require("express");
 
 var app = express();
 var PORT = 1227;
-
+app.get("/", function(req, res){
+   res.sendfile(__dirname + '/index.html');
+});
 app.listen(PORT, function(req, res){
    console.log("Go to URL :- localhost:" + PORT);
 });
